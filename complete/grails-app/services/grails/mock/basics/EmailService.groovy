629a6b6d1649f4eed4f81db1766b3c50b05369cf
sdelamo <sergio.delamo@softamo.com>
2017-04-18 08:46:04 +0200

    Annotate with compile static if possible

--- complete/grails-app/services/grails/mock/basics/EmailService.groovy
+++ complete/grails-app/services/grails/mock/basics/EmailService.groovy
@@ -1,8 +1,10 @@
 package grails.mock.basics
 
+import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
 
 @Slf4j
+@CompileStatic
 class EmailService {
 
     int sendEmail(Map message) {
