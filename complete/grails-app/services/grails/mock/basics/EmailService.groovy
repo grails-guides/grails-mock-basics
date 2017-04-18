c0805a4ccc039a785146501511ef41785ea3a939
sdelamo <sergio.delamo@softamo.com>
2017-04-18 08:45:52 +0200

    Use logging instead of print statement

--- complete/grails-app/services/grails/mock/basics/EmailService.groovy
+++ complete/grails-app/services/grails/mock/basics/EmailService.groovy
@@ -1,9 +1,12 @@
 package grails.mock.basics
 
+import groovy.util.logging.Slf4j
+
+@Slf4j
 class EmailService {
 
     int sendEmail(Map message) {
-        println "Send email: $message"
+        log.info 'Send email: {0}', message
         1
     }
 }
