7027c13759fd3c5b4aef49eddc0c37485aa0659d
sdelamo <sergio.delamo@softamo.com>
2017-04-25 10:51:03 +0200

    unit test for projection query

---
+++ complete/src/test/groovy/grails/mock/basics/ClassroomServiceSpec.groovy
@@ -0,0 +1,33 @@
+package grails.mock.basics
+
+import grails.test.hibernate.HibernateSpec
+import grails.test.mixin.TestFor
+
+@TestFor(ClassroomService)
+class ClassroomServiceSpec extends HibernateSpec {
+
+    List<Class> getDomainClasses() { [Student, Classroom] }
+
+    void "test calculate average grade of classroom"() {
+        when:
+        def classroom = new Classroom(teacher: "Smith")
+        [
+                [name: "Nirav", grade: 91],
+                [name: "Sergio", grade: 95],
+                [name: "Jeff", grade: 93]
+        ].each {
+            classroom.addToStudents(new Student(name: it.name, grade: it.grade))
+        }
+        classroom.save()
+
+        then:
+        Classroom.count() == 1
+        Student.count() == 3
+
+        when:
+        BigDecimal avgGrade = service.calculateAvgGrade("Smith")
+
+        then:
+        avgGrade == 93.0
+    }
+}
